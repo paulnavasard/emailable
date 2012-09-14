5e3f7b2c43d0c8a7bde0cad4f62fc90facb8bfb1
Jason Staten <jstaten07@gmail.com>
2012-09-14 10:20:40 -0600

    change api a bit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var validator = require('./lib/validator');
 
-var v = validator('kjklasdjklk@gmail.com');
+var v = validator('adfasdfsfdasfd83843@gmail.com', function(err, valid) {
+  console.log('Valid: ' + valid);
+});
 v.on('resolveMx', function(hosts) { console.dir(hosts); });
 v.on('connect', function() { console.log('connected'); });
 v.on('error', function(err) { console.error(err); });
-v.run(function(err, valid){
-  console.log('Valid: ' + valid);
-});
