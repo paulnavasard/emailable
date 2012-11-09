077fe5beb715ff91fdbae8bb288cda8fd96ded54
Paul Navasard <pnavasard@gmail.com>
2012-11-09 16:14:40 -0700

    Chnaged results to an object

--- test/validator-test.js
+++ test/validator-test.js
@@ -4,15 +4,30 @@ var should = require('chai').should()
 describe('test', function() {
   it('should validate a good email', function(done) {
     validator('pnavasard@katalus.com', function(err, result) {
-      result.should.equal(true);
+      result.valid.should.be.true;
+      done();
+    });
+  });
+
+  it('should error out on an invalid email address', function(done) {
+    validator('pnavasard@katalus', function(err, result, message) {
+      result.valid.should.be.false;
+      result.message.should.match(/invalid email address/i);
       done();
     });
   });
 
   it('should error out on valid domain invalid user', function(done) {
     validator('pnavasard4@katalus.com', function(err, result) {
-      console.log(err);
-      result.should.equal(false);
+      result.valid.should.be.false;
+      done();
+    });
+  });
+
+  it('should error on invalid domain', function(done) {
+    validator('pnavasard@katalusbuttmunch.com', function(err, result, message) {
+      result.valid.should.be.false;
+      result.message.should.match(/enotfound/i);
       done();
     });
   });
